6f4766ed896a8887d65a41d79910693da3e62f2f
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-09-03 22:00:59 -0700

    refactor: update page query mock pattern

--- src/pages/__tests__/index.test.js
+++ src/pages/__tests__/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { StaticQuery } from 'gatsby';
 
-import mockPageQueries from '../../__mocks__/query-mocks';
+import mockPageQueries from '../../page-query-mocks';
 
 import IndexPage from '../index';
 
